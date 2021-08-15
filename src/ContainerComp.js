21e28cee39c52508abd09c3050a3a8ecdb12d123
omar-he <omar.hendi92@gmail.com>
2021-08-15 11:55:41 +0300

    Comments to improve code

--- src/ContainerComp.js
+++ src/ContainerComp.js
@@ -1,26 +1,22 @@
-import react, { useEffect, useState } from 'react';
-import {getImages} from './requestApi';
-import ImageComponent from './ImagesComponent';
+import react, { useEffect, useState } from "react";
+import { getImages } from "./requestApi";
+import ImageComponent from "./ImagesComponent";
 
-const ContainerComp =()=>{
+const ContainerComp = () => {
+  const [dataList, setDataList] = useState([]);
 
-    const   [dataList, setDataList] = useState([]);
-
-    const getImagesData = async ()=>{
-        
+  const getImagesData = async () => {
     const response = await getImages();
     setDataList(response.results);
-    };
+  };
 
-    useEffect(() => {
-        console.log("effect");
-        getImagesData();
-        }, []);
+  useEffect(() => {
+    console.log("effect");
+    getImagesData();
+  }, []);
 
-        console.log(dataList);
-    return (
-        <ImageComponent data={dataList}/>
-      
-    );
+  console.log(dataList);
+  //   put a condition here to check if there is images or not (conditional render)
+  return <ImageComponent data={dataList} />;
 };
 export default ContainerComp;
--- src/ImagesComponent.js
+++ src/ImagesComponent.js
@@ -1,13 +1,33 @@
-import react from 'react';
-import ImgCardComp from './ImgCardComp';
+import react from "react";
+import ImgCardComp from "./ImgCardComp";
 
-const ImageComponent =(props) =>{
-  console.log("img comp log",props.data);
-    const images = props.data.map(image =>{
-        return(<ImgCardComp key={image.id} image={image.urls.regular}/>)
-        
-    });
-    
-    return(<div className="ImagesListStyle">{images}</div>)
+// This is easier to read and better for the performance, no need to create a component
+// for the image, you can use thte HTML tag directly here
+
+// const ImageComponent = ({ data }) => {
+//   return (
+//     <div className="ImagesListStyle">
+//       {data.length > 0 &&
+//         data.map((image) => {
+//           return (
+//             <img
+//               key={image.id}
+//               className="imgStyle"
+//               alt="alt"
+//               src={image.urls.regular}
+//             />
+//           );
+//         })}
+//     </div>
+//   );
+// };
+
+const ImageComponent = (props) => {
+  console.log("img comp log", props.data);
+  const images = props.data.map((image) => {
+    return <ImgCardComp key={image.id} image={image.urls.regular} />;
+  });
+
+  return <div className="ImagesListStyle">{images}</div>;
 };
 export default ImageComponent;
