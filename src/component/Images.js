d5e883f697435244859e85a3ad65b05179227833
Rama Khder <U7173@ISOINT.NET>
2021-09-06 08:59:24 +0300

    add ImagCard

--- src/component/Images.js
+++ src/component/Images.js
@@ -1,11 +1,23 @@
 import react from "react";
-
+import { useState } from "react";
+import ImgCard from "./ImgCard";
 
 // This is easier to read and better for the performance, no need to create a component
 // for the image, you can use thte HTML tag directly here
 
 const Images = ({ data }) => {
+
+  const [Img, setImg] = useState();
+
+  const onSelectImg=(item)=>{
+    console.log("on select",item);
+    setImg(item);
+    console.log("//// ",Img);
+}
+
+  console.log(data);
   return (
+    <div>
     <div className="images-list">
       {data.length > 0 &&
         data.map((image) => {
@@ -14,10 +26,17 @@ const Images = ({ data }) => {
               key={image.id}
               className="img-style"
               alt="alt"
-              src={image.urls.regular}
+              src={image.urls.thumb}
+              onClick={()=>onSelectImg(image)}
             />
           );
         })}
+    </div>
+  
+    <div>
+    { Img  &&
+      <ImgCard image={Img}/>}
+    </div>
     </div>
   );
 };
--- src/component/ImgCard.js
+++ src/component/ImgCard.js
@@ -3,7 +3,8 @@ import react from 'react';
 const ImgCard = ({image}) =>{
     
     console.log("img" ,image);
-    return(<img className="img-style" src={image}/>);
+
+    return(<img  className="img-style" src={image.urls.thumb}/>);
     
 };
 
